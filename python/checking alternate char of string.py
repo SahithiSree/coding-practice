f673224cba0ed50954ba1852668659199b88a14d
Sahithi sree <62046696+SahithiSree@users.noreply.github.com>
2021-12-24 23:05:52 +0530

    Create checking alternate char of string.py

---
+++ python/checking alternate char of string.py
@@ -0,0 +1,21 @@
+#we check for an alternative postions characters or same starting from index 0
+
+def mobilevirus(arr):
+    for a in arr:
+        good=1
+        k=a[0]
+        for i in range(0,len(a),+2):
+            if k!=a[i]:
+                good=0
+                break
+        print(good)
+arr=list(map(str,input().split()))
+mobilevirus(arr)
+
+#input:['abadam',' madam','kljlil']
+#output:
+'''
+1
+0
+0
+'''
