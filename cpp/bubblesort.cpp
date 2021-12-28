02e59c7970e52bcec13de4f1fde360cbc6b5db1a
Sahithi sree <62046696+SahithiSree@users.noreply.github.com>
2021-12-28 18:42:19 +0530

    Create bubblesort.cpp

---
+++ cpp/bubblesort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+void bubbleSort(int array[], int size) {
+  for (int step = 0; step < size; ++step) {
+    for (int i = 0; i < size - step; ++i) {
+      if (array[i] > array[i + 1]) {
+        int temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+      }
+    }
+  }
+  for (int i = 0; i < size; ++i) {
+    cout << "  " << array[i];
+  }
+  cout << "\n";
+}
+
+int main() {
+  int data[] = {-2, 45, 0, 11, -9};
+  int size = sizeof(data) / sizeof(data[0]);
+  bubbleSort(data, size);
+
+}
+
